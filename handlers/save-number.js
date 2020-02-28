9d772b4ba288cf57f4b3a0642ce0d7b82d3c0063
Rahel <rahelsemukora@gmail.com>
2020-02-28 18:55:18 +0100

    adding save and reset

--- handlers/save-number.js
+++ handlers/save-number.js
@@ -1,14 +1,40 @@
 function saveNumberHandler() {
   debugger;
   // read new number from user input
-
+  const newInput = document.getElementById('input').value;
+  
+   current = newInput;
   // read from state the data you will need for the next step
+ // const newCurrent = numbers.current;
 
   // find the new biggest and smallest values
+  if(current >= 0){
+  numbers.biggest = current;
+  numbers.smallest = numbers.smallest;
+
+/*  numbers.push(numbers.biggest);
+  numbers.biggest = newBiggest;*/
+    
+  }else if(current < 0){
+    numbers.smallest = current;
+    numbers.biggest = numbers.biggest;
+
+  /*numbers.push(numbers.smallest);
+    numbers.smallest = newSmallest;*/
+ 
+  }
 
   // update state: new biggest, new smallest, new current & save the last current
 
+ //numbers.past.push(numbers.current);
+   //numbers.current = newCurrent;
+
+ 
   // re-render the user interface with values stored in state
+
+  document.getElementById('input').innerHTML = numbers.current;
+  document.getElementById('biggest').innerHTML = numbers.biggest;
+  document.getElementById('smallest').innerHTML = numbers.smallest;
 
   // log user interaction: handler name, user input, new state
   log.push({
